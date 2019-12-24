b23560ac4b90ebf1541f0174511bd1258f7281ea
dmishra2 <Dheeraj.Mishra2@harman.com>
2019-12-24 23:11:13 +0530

    updated , redundant code

--- Loan_flow_machine/loan_activator.py
+++ Loan_flow_machine/loan_activator.py
@@ -1,41 +1,7 @@
 from State_Model_Container.LoanProcess import LoanProcess
 from random import randint
 from time import clock
-#from State_Model_Container.Transitions import LoanAction
-
-
-class LoanAction:
-
-    def __init__(self, action):
-        self.action = action
-
-    def __str__(self):
-        return self.action
-
-    def __eq__(self, other):
-        return self.action == other.action
-
-    def __ne__(self, other):
-        return self.action != other.action
-
-    def __hash__(self):
-        return hash(self.action)
-
-
-
-
-
-LoanAction.loanCriteriaNotMet = LoanAction("criteria not met")
-LoanAction.submitted = LoanAction("submitted")
-LoanAction.documentVerification = LoanAction("started verifying document")
-LoanAction.showDocument = LoanAction("need more document")
-LoanAction.backgroundCheck = LoanAction("customer background check")
-LoanAction.conditionClear = LoanAction("all condition met")
-LoanAction.Clearance = LoanAction("loan provided")
-LoanAction.conditionNotMet = LoanAction("condition not met")
-LoanAction.re_apply = LoanAction("loan re apply")
-LoanAction.checkEligibilty = LoanAction("check eligibility")
-LoanAction.rejected = LoanAction("loan disapproved")
+from State_Model_Container.Transitions import LoanAction
 
 
 def transform_input_to_action(input):
